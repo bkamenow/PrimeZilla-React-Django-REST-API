c7a3a747ebef8aadaa10e6356134ccbdb2825481
Borislav Kamenov <kamenowink@gmail.com>
2023-11-19 18:04:21 +0200

    put our shops in carousel

--- frontend/src/components/Carousel/CarouselCards.jsx
+++ frontend/src/components/Carousel/CarouselCards.jsx
@@ -1,52 +1,41 @@
+import { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
+import { getAllShops } from "../../services/shopService";
 
 export default function CarouselCards() {
+    const [shops, setShops] = useState([]);
+
+    useEffect(() => {
+        const fetchShops = async () => {
+            try {
+                const allShops = await getAllShops();
+                const firstFiveShops = allShops.slice(0, 5);
+                setShops(firstFiveShops);
+            } catch (error) {
+                console.error("Error fetching shops:", error);
+            }
+        };
+
+        fetchShops();
+    }, []);
+
     return (
         <div className='header-container'>
             <div className='carousel-overlay'></div>
             <Carousel>
-                <Carousel.Item>
-                    <img
-                        className='d-block w-100'
-                        src='images/game.png'
-                        alt='First slide'
-                    />
-                    <Carousel.Caption>
-                        <h5>First slide label</h5>
-                        <p>
-                            Nulla vitae elit libero, a pharetra augue mollis
-                            interdum.
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className='d-block w-100'
-                        src='/images/harry.jpg'
-                        alt='Second slide'
-                    />
-                    <Carousel.Caption>
-                        <h5>Second slide label</h5>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit.
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className='d-block w-100'
-                        src='/images/lord.png'
-                        alt='Third slide'
-                    />
-                    <Carousel.Caption>
-                        <h5>Third slide label</h5>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl
-                            consectetur.
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+                {shops.map((shop) => (
+                    <Carousel.Item key={shop.id}>
+                        <img
+                            className='d-block w-100'
+                            src={shop.image_url}
+                            alt={`Shop ${shop.name}`}
+                        />
+                        <Carousel.Caption>
+                            <h5>{shop.name}</h5>
+                            <p>{shop.description}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
             </Carousel>
         </div>
     );
