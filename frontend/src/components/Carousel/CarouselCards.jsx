0a0341cc3c9adaa0d5ecb21466baa29a04d53285
Borislav Kamenov <kamenowink@gmail.com>
2023-11-19 20:24:37 +0200

    made carousel shop image clickable with url to shop items

--- frontend/src/components/Carousel/CarouselCards.jsx
+++ frontend/src/components/Carousel/CarouselCards.jsx
@@ -1,22 +1,26 @@
 import { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { getAllShops } from "../../services/shopService";
+import { Link } from "react-router-dom";
 
 export default function CarouselCards() {
     const [shops, setShops] = useState([]);
 
     useEffect(() => {
-        const fetchShops = async () => {
+        const fetchShopsWithItems = async () => {
             try {
                 const allShops = await getAllShops();
-                const firstFiveShops = allShops.slice(0, 5);
+                const shopsWithItems = allShops.filter(
+                    (shop) => shop.items.length > 0
+                );
+                const firstFiveShops = shopsWithItems.slice(0, 5);
                 setShops(firstFiveShops);
             } catch (error) {
                 console.error("Error fetching shops:", error);
             }
         };
 
-        fetchShops();
+        fetchShopsWithItems();
     }, []);
 
     return (
@@ -25,11 +29,13 @@ export default function CarouselCards() {
             <Carousel>
                 {shops.map((shop) => (
                     <Carousel.Item key={shop.id}>
-                        <img
-                            className='d-block w-100'
-                            src={shop.image_url}
-                            alt={`Shop ${shop.name}`}
-                        />
+                        <Link to={`/items/${shop.id}`}>
+                            <img
+                                className='d-block w-100'
+                                src={shop.image_url}
+                                alt={`Shop ${shop.name}`}
+                            />
+                        </Link>
                         <Carousel.Caption>
                             <h5>{shop.name}</h5>
                             <p>{shop.description}</p>
